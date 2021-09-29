f662c05d77dacfc861e3350a59f67ac72c7133af
Omar <ilbuuromar@gmail.com>
2021-09-29 18:35:00 +0300

    Scroll spy

--- detergent/js/detergent-main.js
+++ detergent/js/detergent-main.js
@@ -11,6 +11,44 @@
 
 function swap(element){
   document.getElementById("showcase").src=element.dataset.src;
+  document.getElementById('img-text').classList.add('fadein');
+  document.getElementById('img-text').innerText = element.dataset.caption;
+  setTimeout(function(){
+    document.getElementById('img-text').classList.remove('fadein');
+  },800);
 }
 
+var scroll = new SmoothScroll('a[href*="#"]',{
+  speed:200,
+  header:'[data-scroll-header]'
+});
+
 AOS.init();
+
+
+
+//Change Active element on scroll
+
+var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+  
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+    
+  
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      
+      if (sections[i] <= scrollPosition) {
+        document.getElementsByClassName('.nav-link').classList
+        document.querySelector('a[href*=' + i + ']').classList.add('active');
+      }
+    }
+  };
+
