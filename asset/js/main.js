571c7fee7c71efe9256394657dc212102a482df2
Omar <ilbuuromar@gmail.com>
2021-09-30 09:57:29 +0300

    Uncommented main js

--- asset/js/main.js
+++ asset/js/main.js
@@ -14,59 +14,59 @@ document.onreadystatechange = function () {
   }
 }
 
-// window.addEventListener('scroll', function(e) {
-//     var scrollpos = window.scrollY;
-//     if (scrollpos >1 ){
-//       document.getElementById('enterprise-nav').classList.add('bg-light');
-//     }
-//     else{
-//       document.getElementById('enterprise-nav').classList.remove('bg-light');
-//     }
-// });
-// var slideup={
-//   distance:'200%',
-//   origin:'bottom',
-//   opacity:null,
-//   delay:400
-// }
-// var rightReveal={
-//   distance:'200%',
-//   origin:'right',
-//   opacity:null,
-//   delay:400
-// }
-// var lefReveal ={
-//   distance:'200%',
-//   origin:'left',
-//   opacity:null,
-//   delay:400
-// }
-// ScrollReveal().reveal('.slideup',slideup);
-// ScrollReveal().reveal('.right-reveal',rightReveal);
-// ScrollReveal().reveal('.left-reveal',lefReveal);
+window.addEventListener('scroll', function(e) {
+    var scrollpos = window.scrollY;
+    if (scrollpos >1 ){
+      document.getElementById('enterprise-nav').classList.add('bg-light');
+    }
+    else{
+      document.getElementById('enterprise-nav').classList.remove('bg-light');
+    }
+});
+var slideup={
+  distance:'200%',
+  origin:'bottom',
+  opacity:null,
+  delay:400
+}
+var rightReveal={
+  distance:'200%',
+  origin:'right',
+  opacity:null,
+  delay:400
+}
+var lefReveal ={
+  distance:'200%',
+  origin:'left',
+  opacity:null,
+  delay:400
+}
+ScrollReveal().reveal('.slideup',slideup);
+ScrollReveal().reveal('.right-reveal',rightReveal);
+ScrollReveal().reveal('.left-reveal',lefReveal);
 
 
-// $(document).ready(function(){
-//   $(".owl-carousel").owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//       0:{
-//         items:1
-//       },
-//       600:{
-//         items:3
-//       },
-//       1000:{
-//         items:5
-//       }
-//     }
-//   });
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  });
 
-// }
+}
 
 
 
-// );
+);
 AOS.init()
