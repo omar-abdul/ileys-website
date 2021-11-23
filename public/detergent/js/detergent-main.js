ea4c8dab17a850cad4063e768da619536acd5d5d
Omo <ilbuuromar@gmail.com>
2021-11-24 00:46:30 +0300

    updating scrollspy

--- public/detergent/js/detergent-main.js
+++ public/detergent/js/detergent-main.js
@@ -34,25 +34,25 @@ Array.prototype.forEach.call(section, function (e) {
   sections[e.id] = e.offsetTop;
 });
 
-window.onscroll = function () {
-  var scrollPosition =
-    document.body.scrollTop || document.documentElement.scrollTop;
+// window.onscroll = function () {
+//   var scrollPosition =
+//     document.body.scrollTop || document.documentElement.scrollTop;
 
-  if (scrollPosition < 30) {
-    var navlink = document.querySelectorAll("#menu-items>.nav-link");
+//   if (scrollPosition < 30) {
+//     var navlink = document.querySelectorAll("#menu-items>.nav-link");
 
-    for (var k = 0; k < navlink.length; k++) {
-      navlink[k].classList.remove("active");
-    }
-  }
+//     for (var k = 0; k < navlink.length; k++) {
+//       navlink[k].classList.remove("active");
+//     }
+//   }
 
-  for (i in sections) {
-    if (sections[i] <= scrollPosition) {
-      if (document.querySelector(".nav-link.active")) {
-        document.querySelector(".nav-link.active").classList.remove("active");
-      }
+//   for (i in sections) {
+//     if (sections[i] <= scrollPosition) {
+//       if (document.querySelector(".nav-link.active")) {
+//         document.querySelector(".nav-link.active").classList.remove("active");
+//       }
 
-      document.querySelector("a[href*=" + i + "]").classList.add("active");
-    }
-  }
-};
+//       document.querySelector("a[href*=" + i + "]").classList.add("active");
+//     }
+//   }
+// };
