675ab8179b8513a08cbc3e1a4efa9e8fcd4ac452
Omar <ilbuuromar@gmail.com>
2022-03-23 11:18:44 +0300

    Added Parcel

--- src/app.js
+++ src/app.js
@@ -3,34 +3,39 @@ import ReactDOM from "react-dom";
 import { Buttons } from "./Buttons";
 import Cards from "./Cards";
 
+import detergent from "./asset/img/all-detergent-resized.jpg"
+import washingMachine from  "./asset/img/washing-machine-powder.jpg";
+import liquidProducts from "./asset/img/liquid-products.jpg";
+import saafi from "./asset/img/Safi-Soap.jpg";
+
 const App = () => {
   const arr = [
     {
       id: 2,
       color: "bg-danger text-white",
       productName: "Detergent Powders",
-      imgSrc: "../asset/img/all-detergent-resized.jpg",
-      text: "A powerful triple formula powder meant to fight the toughest of stains",
+      imgSrc: detergent,
+      text: "A powerful triple formula powder meant to fight the toughest of stai",
     },
     {
       id: 3,
       color: "bg-light-blue text-white",
       productName: "Washing Machine Powders",
-      imgSrc: "../asset/img/washing-machine-powder.jpg",
+      imgSrc:washingMachine,
       text: "Leave your dirty works to our Washing Machine Powder ",
     },
     {
       id: 4,
       color: "bg-pink",
       productName: "Liquid Detergents",
-      imgSrc: "../asset/img/liquid-products.jpg",
+      imgSrc: liquidProducts,
       text: "Whatever the stain, Our strong yet gentle variable Liquid Detergents will take care of it",
     },
     {
       id: 5,
       color: "bg-damp-yellow",
       productName: "Laundry Soap",
-      imgSrc: "../asset/img/Safi-Soap.jpg",
+      imgSrc: saafi,
       text: "Safi Soap sets the bar cleaning the toughest of slime",
     },
   ];
@@ -54,7 +59,8 @@ const App = () => {
           activeId={infoCard.id}
         />
       </div>
-      <div className="card-container" data-aos="fade-right">
+      <div className="card-container" >
+        
         <Cards
           bgColor={infoCard.color}
           text={infoCard.text}
